dd6babc47b15d9203163dc8e633b8a084bd4fcd7
Richard Shade <rshade@rightscale.com>
2013-10-11 14:28:29 -0500

    changing @new to current

--- providers/default.rb
+++ providers/default.rb
@@ -92,12 +92,12 @@ action :generate_settings do
     group "root"
     mode "0644"
     variables(  :database_driver => current_resource.database_driver,
-                :database_schema => @new_resource.database_schema,
-                :database_user => @new_resource.database_user,
-                :database_password => @new_resource.database_password,
-                :database_host => @new_resource.database_host,
-                :database_port => @new_resource.database_port,
-                :database_table_prefix => @new_resource.database_table_prefix )
+                :database_schema => current_resource.database_schema,
+                :database_user => current_resource.database_user,
+                :database_password => current_resource.database_password,
+                :database_host => current_resource.database_host,
+                :database_port => current_resource.database_port,
+                :database_table_prefix => current_resource.database_table_prefix )
     action :create
   end
 end
